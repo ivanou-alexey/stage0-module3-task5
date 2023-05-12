f99d8eb136858827c9fdd1b65107fdaa54d3b67c
Alexey Ivanov <ai5973861@gmail.com>
2023-05-12 18:57:18 +0300

    complete

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = 60 * minutes;
+        System.out.println(seconds);
     }
 }
