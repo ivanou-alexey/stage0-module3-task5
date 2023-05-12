679d40aa913adcc0146fe7e84df2f67532182da1
Alexey Ivanov <ai5973861@gmail.com>
2023-05-12 19:05:30 +0300

    complete

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         float result;
-        result = toBeDivided / 5;
+        result = (float) toBeDivided / 5;
         System.out.println(result);
     }
 }
